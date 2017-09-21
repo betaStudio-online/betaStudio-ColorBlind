68ea111805ef5d180de1edff23097654dae6b454
Samir Dash <sdash@redhat.com>
2017-09-21 09:04:22 +0530

    Add files via upload

---
+++ 0.1/source/betastudio-colorblind.js
@@ -0,0 +1,147 @@
+/*
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                .'                                                                                                                                                                   
+                               @@@@.                                                                                                                                                                 
+                             @@@@@@@@                                                                                                                                                                
+                           ;@@@+  ,@@@@                                                                                                                                                              
+                         `@@@@      '@@@;                                                                                                                                                            
+                        @@@@`         @@@@.                                                                                                                                                          
+                      +@@@:            `@@@@                                                                                                                                                         
+                    ,@@@#                ,@@@#                                                                                                                                                       
+                  `@@@@                    +@@@:                                                                                                                                                     
+                 @@@@.                       @@@@`                                                                                                                                                   
+               '@@@'                          `@@@@                                                                                                                                                  
+             .@@@@                              :@@@+                                                                                                                                                
+            @@@@`                                 #@@@,                                                                                                                                              
+           @@@:                                     @@@`                                                                                                                                             
+           @@@@`                                  `@@@@`                                                                                                                                             
+           @@@@@@                                @@@@@@`                                                                                                                                             
+           @@ :@@@#                            +@@@: @@`                                                                                                                                             
+           @@   +@@@;                        ,@@@#   @@.        @@@,                                                    :@:  :;                                    ::   :@                           
+           @@     @@@@,                    `@@@@     @@.        @@@`                            :@@                    ;;                                          ::                                
+           @@      `@@@@                  @@@@`      @@.        @@@                            .@@@                    @           :.                              :.                                
+           @@        ,@@@@              ;@@@;        @@.        @@@                            :@@@                    @           :`                              :`                                
+           @@          +@@@:          `@@@@          @@.       `@@@@@@@@@+         @@@@@@    :@@@@@@@@    ;@@@@@@@     @          @@@@@@  +`         @       '@@@@@@    .+      @@@@@,               
+           @@            @@@@.       @@@@`           @@.       `@@@@@@@@@@@`     #@@@@@@@@+  :@@@@@@@@   @@@@@@@@@@'   ##          #      #          @     +@      +    :;    #'     .@              
+           @@             `@@@@    #@@@,             @@.       .@@@@@@@@@@@@    #@@@@  @@@@` ;@@@@@@@@   @:     @@@@    @+         @      #          @    #:       +    ::   ':       `#             
+           @@               ,@@@@:@@@+               @@.       :@@@     #@@@#   @@@,    @@@@   @@@@             +@@@     @#        @      @          +   .#        #    :.   @         @             
+           @@                 '@@@@@                 @@.       :@@@      @@@@  #@@@     `@@@   @@@@             @@@@      #@       @      @          ;   @         @    ;`  ,;         ;.            
+           @@                   @@`                +@@@,       '@@@      #@@@  @@@@@@@@@@@@@   @@@#          :@@@@@@       :@      @      @         `:   @         @    +   @          .'            
+           @@                   @@               ,@@@@@,       #@@@      ;@@@  @@@@@@@@@@@@@   @@@+       @@@@@@@@@@        .@     @      @         .:  .+         @    #   @           #            
+           @@                   @@             `@@@@ @@,       @@@@      #@@@  @@@;            @@@;     +@@@@@: +@@@         .@    @      @         :.  ::         @    @   @          .;            
+           @@                   @@            @@@@,  @@,       @@@@      @@@@  @@@#            @@@;    .@@@#    #@@@          #:   @      @         +   .;         @    @   @          '.            
+           @@                   @@          '@@@+    @@,       @@@@     `@@@+  @@@@            @@@;    @@@@     @@@@           @   @      @         @    @         @    @   ::         @             
+           @@                   @@        .@@@@      @@,       @@@@@`  `@@@@   `@@@@      :    @@@@    @@@@     @@@@          .+   @      @         #    @        #@    @    @         @             
+           @@                   @@       @@@@`       @@,       @@@;@@@@@@@@;    #@@@@@@@@@@    @@@@@@@ :@@@+  : @@@@          @    @      ;.       @@    .#      `;@    @    ''       @              
+           @@                   @@     #@@@:         @@,       @@@::@@@@@@'      +@@@@@@@@'    #@@@@@#  @@@@@@' @@@@  @;     @,    @:      #'    :@ @     ,@    '+ @    @     '@    .@               
+           @@                   @@   :@@@#           @@,             :@@:          `;@@@:        #@@:     ;@@:          :@@@:       :@@`     :@@:           :@@:                :@@;                 
+           @@                   @@ `@@@@             @@:                                                                                                                                             
+           @@                   @@@@@@.              @@:                                                                                                                                             
+           @@                   @@@@'                @@:                                                                                                                                             
+           @@@+                 @@@                ;@@@:                                                                                                                                             
+            #@@@:               @@               `@@@@                                                                                                                                               
+              @@@@`             @@              @@@@.                                                                                                                                                
+               `@@@@            @@            +@@@'                                                                                                                                                  
+                 :@@@+          @@          ,@@@@                                                                                                                                                    
+                   #@@@,        @@         @@@@`                                                                                                                                                     
+                     @@@@`      @@`      @@@@,                                                                                                                                                       
+                      .@@@@     @@`    ;@@@+                                                                                                                                                         
+                        ;@@@+   @@`  `@@@@                                                                                                                                                           
+                          #@@@: @@` @@@@`                                                                                                                                                            
+                            @@@@@@#@@@:                                                                                                                                                              
+                             `@@@@@@#                                                                                                                                                                
+                               :@@@                                                                                                                                                                  
+                                 .                                                                                                                                                                   
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+
+
+var betaStudio_module_colorBlind_config = {
+    doc:         document,/* target document (with a body tag )object to apply */
+    testMode:    false /* valid values are true|false */
+}
+var betaStudio_module_colorBlind=(function(config){
+/*********************************betaStudioModule Info*********************************/
+this.module_name = "betaStudio-ColorBlind";
+this.module_description = "This module helps in testing a web page for color blind effects."
+this.module_license_type = "MIT";
+this.module_dependancies = "(No Dependancies)";/* if any dependancies like jquery etc, metion here as an info */
+this.module_version = "0.1.0";
+this.module_lastupdated = "21-09-2017";
+this.module_authors = "Samir Dash|";/* Additional authors append your name with a pipe (|) as suffix */
+this.module_author_emails = "sdash@redhat.com|";
+this.module_help = "SetNoFilter(doc)|SetProtanopiaFilter(doc)|SetProtanomalyFilter(doc)|SetDeuteranopiaFilter(doc)|SetDeuteranomalyFilter(doc)|SetTritanopiaFilter(doc)|SetTritanomalyFilter(doc)|SetAchromatopsiaFilter(doc)|SetAchromatomalyFilter(doc)|"/* Additional public methods and props append with a pipe (|) as suffix */
+/************************************Config**********************************/    
+if(!config){
+    
+    var config = {
+    doc:document,
+    testMode:true
+}; }else{
+    
+    var config = config;
+    
+}
+/************************************Public Properties*********************************/
+/************************************Public Methods************************************/
+this.SetNoFilter = function(_doc){if(!_doc){var _doc = document; }return setfilter(null, _doc);}
+this.SetProtanopiaFilter =  function(doc){return _private_SetFilter("protanopia", doc);}
+this.SetProtanomalyFilter = function(doc){return _private_SetFilter("protanomaly", doc);}
+this.SetDeuteranopiaFilter = function(doc){return _private_SetFilter("deuteranopia", doc);}
+this.SetDeuteranomalyFilter = function(doc){return _private_SetFilter("deuteranomaly", doc);}
+this.SetTritanopiaFilter = function(doc){return _private_SetFilter("tritanopia", doc);}
+this.SetTritanomalyFilter = function(doc){return _private_SetFilter("tritanomaly", doc);}
+this.SetAchromatopsiaFilter = function(doc){return _private_SetFilter("achromatopsia", doc);}
+this.SetAchromatomalyFilter = function(doc){return _private_SetFilter("achromatomaly", doc);}
+/*******************************Private Properties**************************************/
+var _private_colorblind_str = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanopia"> <feColorMatrix type="matrix" values="0.567,0.433,0,0,0 0.558,0.442,0,0,0 0 0.242,0.758,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> <filter id="protanomaly"> <feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> <filter id="deuteranopia"> <feColorMatrix type="matrix" values="0.625,0.375,0,0,0 0.7,0.3,0,0,0 0,0.3,0.7,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> <filter id="deuteranomaly"> <feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> <filter id="tritanopia"> <feColorMatrix type="matrix" values="0.95,0.05,0,0,0 0,0.433,0.567,0,0 0,0.475,0.525,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> <filter id="tritanomaly"> <feColorMatrix type="matrix" values="0.967,0.033,0,0,0 0,0.733,0.267,0,0 0,0.183,0.817,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> <filter id="achromatopsia"> <feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> <filter id="achromatomaly"> <feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix> </filter> </defs> </svg>';
+/*******************************Private Properties**************************************/
+var _private_SetFilter = function(_filtername, _doc){
+if(!_doc){var _doc = document; }
+if(_filtername == null){
+document.body.style.filter="";
+}else{
+document.body.style.filter="url(#"+_filtername+")";
+}return true; 
+}
+var _private_InitFilterAsset = function(_doc){
+if(!_doc){var _doc = document; }
+var betaStudio_module_colorBlind_tempDiv = document.createElement("DIV");
+betaStudio_module_colorBlind_tempDiv.id="betaStudio_colorblind";
+betaStudio_module_colorBlind_tempDiv.style.display = "none";
+betaStudio_module_colorBlind_tempDiv.innerHTML = _private_colorblind_str;
+document.body.appendChild(betaStudio_module_colorBlind_tempDiv);
+}
+var _private_Init = function(_doc, _testMode){
+    if(!_doc){var doc = document; }
+    _private_InitFilterAsset(_doc);
+    if(_testMode){
+        console.log("***********************************************************************************************************************************");
+        console.log("betaStudio::"+module_name+" [Initiated with testMode as true.]");
+        console.log("module_version:"+module_version+"");
+        console.log("module_lastupdated:"+module_lastupdated+"");
+        console.log("module_license_type:"+module_license_type+"");
+        console.log("module_description:"+module_description+"");
+        console.log("module_authors:"+module_authors+"");
+        console.log("module_author_emails:"+module_author_emails+"");
+        console.log("module_help:"+module_help+"");
+        console.log("***********************************************************************************************************************************");
+    }
+}
+
+_private_Init(config.doc, config.testMode);
+return this;
+}(betaStudio_module_colorBlind_config));
+
+betaStudio_module_colorBlind.SetAchromatopsiaFilter();
